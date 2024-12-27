2abc0afeabc12f0207e572605100e40540389e50
iamNzninZhra <iamnazaninzahra2002@gmail.com>
2024-12-27 18:40:54 +0330

    Interface

---
+++ DomainLayer/InterFaces/IRemainInStock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.InterFaces
+{
+    public interface IRemainInStock
+    {
+        public int StockID (int stockID);
+        public string ProductSerialNumber (int productID);
+        public int Count (int stockID);
+    }
+}
