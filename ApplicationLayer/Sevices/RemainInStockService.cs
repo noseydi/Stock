d93d5dac7b6fdcb83c9e1ae1ab5ff078a7c0b5e7
iamNzninZhra <iamnazaninzahra2002@gmail.com>
2024-12-27 18:51:07 +0330

    create service

---
+++ ApplicationLayer/Sevices/RemainInStockService.cs
@@ -0,0 +1,27 @@
+﻿using DomainLayer.InterFaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationLayer.Sevices
+{
+    internal class RemainInStockService : IRemainInStock
+    {
+        public int Count(int stockID, int productQuantity, char dimensions)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ProductSerialNumber(int productID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int StockID(int stockID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
